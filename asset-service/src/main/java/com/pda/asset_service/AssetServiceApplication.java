40b1f6fa698c5a543050507ed8606a0297318951
seohee99 <imshee99@gmail.com>
2024-06-17 12:31:05 +0900

    chore: investment-service 모듈 분리

--- asset-service/src/main/java/com/pda/asset_service/AssetServiceApplication.java
+++ asset-service/src/main/java/com/pda/asset_service/AssetServiceApplication.java
@@ -1,12 +1,13 @@
-package com.pda.investment_service;
+package com.pda.asset_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication(scanBasePackages = "com.pda")
-public class InvestmentServiceApplication {
+public class AssetServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InvestmentServiceApplication.class, args);
+		SpringApplication.run(AssetServiceApplication.class, args);
 	}
 
 }
--- asset-service/src/test/java/com/pda/asset_service/AssetServiceApplicationTests.java
+++ asset-service/src/test/java/com/pda/asset_service/AssetServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pda.investment_service;
+package com.pda.asset_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InvestmentServiceApplicationTests {
+class AssetServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
