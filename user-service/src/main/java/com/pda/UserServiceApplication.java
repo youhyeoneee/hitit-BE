745543cefb6b99704aee81fa3d23bab7877ed302
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-18 17:45:34 +0900

    !HOTFIX: @EnableJpaRepositories로 basePackage 명시
    #30

--- user-service/src/main/java/com/pda/UserServiceApplication.java
+++ user-service/src/main/java/com/pda/UserServiceApplication.java
@@ -2,8 +2,10 @@ package com.pda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.pda")
+@EnableJpaRepositories(basePackages = {"com.pda.user_service", "com.pda.investment_test"})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
