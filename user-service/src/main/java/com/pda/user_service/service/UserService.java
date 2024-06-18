99a6930cf9d82781f25c04f5bad7d13c1b248050
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-18 13:29:39 +0900

    fix: 헤더에 있는 토큰 읽기
    Bearer ~ 로 시작하는 토큰을 읽어 로그로 출력
    
    #1

--- user-service/src/main/java/com/pda/user_service/service/UserService.java
+++ user-service/src/main/java/com/pda/user_service/service/UserService.java
@@ -17,6 +17,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,7 +30,7 @@ import java.util.Optional;
 @Slf4j
 @Service
 @PropertySource(value = {"env.properties"})
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
     @Autowired
@@ -118,5 +121,11 @@ public class UserService {
         else {
             throw new NotCorrectPasswordException("비밀번호가 일치하지 않습니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findById(Integer.valueOf(username));
+        return user.orElseThrow();
     }
 }
