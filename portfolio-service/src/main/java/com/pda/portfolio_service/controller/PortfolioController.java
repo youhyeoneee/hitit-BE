818dc09fd93aeabb875f49acab8f748129ec65b0
SEO-YJ <tjdbwns19@naver.com>
2024-06-18 14:18:55 +0900

    feat: 자체 서비스 포트폴리오 전체 조회 Controller 구현
    hitit-db-portfolio DB의 private_portfolios 테이블 데이터 전체 조회 Controller 개발

--- portfolio-service/src/main/java/com/pda/portfolio_service/controller/PortfolioController.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/controller/PortfolioController.java
@@ -1,36 +1,27 @@
 package com.pda.portfolio_service.controller;
 
+import com.pda.portfolio_service.dto.HititResponseDto;
 import com.pda.portfolio_service.service.PortfolioService;
-import com.pda.security.JwtTokenProvider;
-import com.pda.user_service.dto.LoginDto;
-import com.pda.user_service.dto.LoginResponseDto;
-import com.pda.user_service.dto.SignupUserDto;
-import com.pda.user_service.dto.UserInfoDto;
-import com.pda.user_service.jpa.User;
-import com.pda.user_service.service.UserService;
 import com.pda.utils.api_utils.ApiUtils;
-import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import static com.pda.utils.api_utils.ApiUtils.error;
+import java.util.List;
+
 import static com.pda.utils.api_utils.ApiUtils.success;
 
 @RestController
 @Slf4j
 @RequestMapping("/api/portfolios")
-@PropertySource(value = {"env.properties"})
 public class PortfolioController {
     @Autowired
     private PortfolioService portfolioService;
 
     @GetMapping("/hitit")
-    public ApiUtils.ApiResult getHititPortfolio() {
-         HititResponseDto hititResponseDto = portfolioService.getHititPortfolio();
-        return success(HititResponseDto);
+    public ApiUtils.ApiResult<List<HititResponseDto>> getHititPortfolios() {
+        List<HititResponseDto> hititResponseDto = portfolioService.getHititPortfolios();
+        return success(hititResponseDto);
     }
 }
--- portfolio-service/src/main/java/com/pda/portfolio_service/service/PortfolioService.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/service/PortfolioService.java
@@ -1,25 +1,33 @@
 package com.pda.portfolio_service.service;
 
-import com.pda.portfolio_service.jpa.PortfolioRepository;
-import com.pda.user_service.dto.SignupUserDto;
-import com.pda.user_service.jpa.User;
-import com.pda.user_service.jpa.UserRepository;
-import com.pda.utils.exception.DuplicatedEmailException;
+import com.pda.portfolio_service.dto.HititResponseDto;
+import com.pda.portfolio_service.jpa.Portfolio;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import com.pda.portfolio_service.jpa.PortfolioRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@PropertySource(value = {"env.properties"})
 public class PortfolioService {
     @Autowired
     private PortfolioRepository portfolioRepository;
 
-    public HititResponseDto getHititPortfolio() {
-        return portfolioRepository.findAll();
+    public List<HititResponseDto> getHititPortfolios() {
+        List<Portfolio> portfolios = portfolioRepository.findAll();
+
+        return portfolios.stream()
+                .map(portfolio -> new HititResponseDto(
+                        portfolio.getId(),
+                        portfolio.getName(),
+                        portfolio.getInvestmentType(),
+                        portfolio.getSummary(),
+                        portfolio.getMinimumSubscriptionFee(),
+                        portfolio.getStockExposure()))
+                .collect(Collectors.toList());
     }
 }
