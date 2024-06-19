b80eabde1774bf5e8fd9bc8f3607cc7790478050
SEO-YJ <tjdbwns19@naver.com>
2024-06-19 11:28:16 +0900

    refactor: 자체 서비스 포트폴리오 선택시 보유 펀드 조회를 위한 PortfolioService 수정
    PortfolioController로부터 전달받은 portfolio_id를 통해 Spring Data JPA와 통신할 getHititPortfoliosFunds 메서드 개발

--- portfolio-service/src/main/java/com/pda/portfolio_service/service/PortfolioService.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/service/PortfolioService.java
@@ -1,7 +1,10 @@
 package com.pda.portfolio_service.service;
 
+import com.pda.portfolio_service.dto.HititPortfoliosFundsResponseDto;
 import com.pda.portfolio_service.dto.HititPortfoliosResponseDto;
 import com.pda.portfolio_service.jpa.Portfolio;
+import com.pda.portfolio_service.jpa.PortfolioFund;
+import com.pda.portfolio_service.jpa.PortfolioFundRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +19,9 @@ public class PortfolioService {
     @Autowired
     private PortfolioRepository portfolioRepository;
 
+    @Autowired
+    private PortfolioFundRepository portfolioFundRepository;
+
     public List<HititPortfoliosResponseDto> getHititPortfolios() {
         List<Portfolio> portfolios = portfolioRepository.findAll();
 
@@ -27,6 +33,20 @@ public class PortfolioService {
                         portfolio.getSummary(),
                         portfolio.getMinimumSubscriptionFee(),
                         portfolio.getStockExposure()))
+                .collect(Collectors.toList());
+    }
+
+    public List<HititPortfoliosFundsResponseDto> getHititPortfoliosFunds(Integer portfolioId) {
+        List<PortfolioFund> fundProducts = portfolioFundRepository.findByIdPortfolioId(portfolioId);
+
+        return fundProducts.stream()
+                .map(portfolioFund -> new HititPortfoliosFundsResponseDto(
+                        portfolioFund.getId().getFundCode(),
+                        portfolioFund.getId().getPortfolioId(),
+                        portfolioFund.getFundName(),
+                        portfolioFund.getFundTypeDetail(),
+                        portfolioFund.getCompanyName(),
+                        portfolioFund.getWeight()))
                 .collect(Collectors.toList());
     }
 }
