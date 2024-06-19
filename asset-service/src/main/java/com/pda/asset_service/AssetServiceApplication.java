4186a33796e2f89d24adf484305fcc15dd675355
seohee99 <imshee99@gmail.com>
2024-06-19 18:09:37 +0900

    feat: EnableJpaRepositories 어노테이션 추가

--- asset-service/src/main/java/com/pda/asset_service/AssetServiceApplication.java
+++ asset-service/src/main/java/com/pda/asset_service/AssetServiceApplication.java
@@ -2,8 +2,12 @@ package com.pda.asset_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = "com.pda")
+@EnableJpaRepositories(basePackages = {"com.pda.asset_service"})
 public class AssetServiceApplication {
 
 	public static void main(String[] args) {
