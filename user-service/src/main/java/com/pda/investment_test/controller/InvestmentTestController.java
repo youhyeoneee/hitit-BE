fae7f578494b5660c8101c724ef7ebcdba9f74d7
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-19 16:25:55 +0900

    feat: 투자성향테스트 질문과 결과 DB에 저장
    #10

--- user-service/src/main/java/com/pda/investment_test/controller/InvestmentTestController.java
+++ user-service/src/main/java/com/pda/investment_test/controller/InvestmentTestController.java
@@ -3,6 +3,7 @@ package com.pda.investment_test.controller;
 import com.pda.investment_test.dto.QuestionDto;
 import com.pda.investment_test.dto.ResultDto;
 import com.pda.investment_test.jpa.answer.Answer;
+import com.pda.investment_test.jpa.user_answer.UserAnswer;
 import com.pda.investment_test.service.InvestmentTestService;
 import com.pda.security.JwtTokenProvider;
 import com.pda.user_service.service.UserService;
@@ -12,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.pda.utils.api_utils.ApiUtils.success;
@@ -36,27 +38,34 @@ public class InvestmentTestController {
 
     @PostMapping("/results")
     public ApiUtils.ApiResult<String> getResults(@RequestBody List<ResultDto> resultDtos, @RequestHeader("Authorization") String bearerToken) {
+        // 토큰 -> user id
+        log.info("bearerToken : " + bearerToken);
+        String token = CustomStringUtils.getToken(bearerToken);
+        int userId = Integer.parseInt(jwtTokenProvider.getUsername(token));
+        log.info("user id : " + userId);
+        List<String> answers = new ArrayList<>();
+        // 점수 계산
         int total_score = 0;
         for(int i=0; i< resultDtos.size(); i++) {
             int question_no = resultDtos.get(i).getQuestion();
             int answer_no = resultDtos.get(i).getAnswer();
             int score = investmentTestService.getScore(question_no, answer_no);
-            log.info(question_no + "번 질문 - " +  answer_no + "번 대답 : 점수 : " + score);
+            log.info(question_no + "번 질문 - " + answer_no + "번 대답 : 점수 : " + score);
             total_score += score;
+            String answer = investmentTestService.getAnswerContent(question_no, answer_no);
+            answers.add(answer);
         }
 
         log.info("total_score : " + total_score);
 
-        // 유저 DB에 타입 저장하기
-        log.info("bearerToken : " + bearerToken);
-        String token = CustomStringUtils.getToken(bearerToken);
-        int userId = Integer.parseInt(jwtTokenProvider.getUsername(token));
-        log.info("user id : " + userId);
+        // user_answers 테이블에 저장
+        UserAnswer userAnswer = investmentTestService.saveUserAnswers(userId, answers);
 
+        // 유저 DB에 타입 저장하기
         String investmentType = investmentTestService.getInvestmentType(total_score).getDescription();
         log.info("investmentType : " + investmentType);
         userService.updateInvestmentTypeAndScore(userId, investmentType, total_score);
-        
+
         return success(investmentType);
     }
 }
--- user-service/src/main/java/com/pda/investment_test/service/InvestmentTestService.java
+++ user-service/src/main/java/com/pda/investment_test/service/InvestmentTestService.java
@@ -6,19 +6,29 @@ import com.pda.investment_test.jpa.answer.Answer;
 import com.pda.investment_test.jpa.answer.AnswerRepository;
 import com.pda.investment_test.jpa.question.Question;
 import com.pda.investment_test.jpa.question.QuestionRepository;
+import com.pda.investment_test.jpa.user_answer.UserAnswer;
+import com.pda.investment_test.jpa.user_answer.UserAnswerRepository;
+import com.pda.user_service.jpa.User;
+import com.pda.user_service.jpa.UserRepository;
 import com.pda.utils.exception.investment_tests.NotFoundQnAException;
 import com.pda.utils.exception.investment_tests.NotFoundQuestionException;
+import com.pda.utils.exception.login.NotFoundUserException;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class InvestmentTestService {
     private QuestionRepository questionRepository;
     private AnswerRepository answerRepository;
+    private UserAnswerRepository userAnswerRepository;
+    private UserRepository userRepository;
 
     public String getQuestionById(int questionNo) throws NotFoundQuestionException {
         Question question = questionRepository.findByNo(questionNo)
@@ -49,6 +59,35 @@ public class InvestmentTestService {
             return InvestmentType.AGGRESSIVE;
         } else {
             return InvestmentType.VERY_AGGRESSIVE;
+        }
+    }
+
+    public String getAnswerContent(int questionNo, int answerNo) throws NotFoundQuestionException {
+        Answer answer = answerRepository.findByQuestionNoAndNo(questionNo, answerNo)
+                .orElseThrow(() -> new NotFoundQnAException(questionNo, answerNo));
+
+        return answer.getContent();
+    }
+
+    public UserAnswer saveUserAnswers(int userId, List<String> answers) throws NotFoundUserException {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundUserException(userId + "번 유저를 찾을 수 없습니다."));
+
+        Optional<UserAnswer> savedUserAnswer = userAnswerRepository.findByUserId(userId);
+
+        if (savedUserAnswer.isEmpty()) {
+            UserAnswer userAnswer = new UserAnswer(userId, answers);
+            userAnswerRepository.save(userAnswer);
+
+            return userAnswer;
+        } else {
+            UserAnswer userAnswer = savedUserAnswer.get();
+            userAnswer.setAnswers(answers);
+            userAnswer.setUpdatedAt(LocalDateTime.now());
+            userAnswerRepository.save(userAnswer);
+
+            return userAnswer;
         }
     }
 }
