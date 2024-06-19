017775c58af8d659348a464001f4612b104af009
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-19 10:16:01 +0900

    rename: questionDto로 클래스 이름 변경
    #10

--- user-service/src/main/java/com/pda/investment_test/controller/InvestmentTestController.java
+++ user-service/src/main/java/com/pda/investment_test/controller/InvestmentTestController.java
@@ -1,19 +1,15 @@
 package com.pda.investment_test.controller;
 
-import com.pda.investment_test.dto.QuestionAndAnswerDto;
+import com.pda.investment_test.dto.QuestionDto;
 import com.pda.investment_test.jpa.answer.Answer;
 import com.pda.investment_test.service.InvestmentTestService;
 import com.pda.utils.api_utils.ApiUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static com.pda.utils.api_utils.ApiUtils.error;
 import static com.pda.utils.api_utils.ApiUtils.success;
 
 @RestController
@@ -25,11 +21,10 @@ public class InvestmentTestController {
     private InvestmentTestService investmentTestService;
 
     @GetMapping("/questions/{no}")
-    public ApiUtils.ApiResult getInvestmentTestQuestionById(@PathVariable int no) {
-
+    public ApiUtils.ApiResult getQuestionById(@PathVariable int no) {
         String content = investmentTestService.getQuestionById(no);
         List<Answer> answers = investmentTestService.getAnswersByQuestionId(no);
-        QuestionAndAnswerDto questionAndAnswerDto = new QuestionAndAnswerDto(content, answers);
-        return success(questionAndAnswerDto);
+        QuestionDto questionDto = new QuestionDto(content, answers);
+        return success(questionDto);
     }
 }
