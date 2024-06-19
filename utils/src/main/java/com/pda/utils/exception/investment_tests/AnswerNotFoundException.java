8c9cbecae6501f3eff16b01e98867128acb5fc6c
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-19 16:38:29 +0900

    rename: 클래스 이름에서 NotFound를 뒤에 나오도록 수정
    #10

--- utils/src/main/java/com/pda/utils/exception/investment_tests/AnswerNotFoundException.java
+++ utils/src/main/java/com/pda/utils/exception/investment_tests/AnswerNotFoundException.java
@@ -1,9 +1,9 @@
 package com.pda.utils.exception.investment_tests;
 
-public class NotFoundQnAException extends RuntimeException {
+public class AnswerNotFoundException extends RuntimeException {
     String message;
 
-    public NotFoundQnAException(int questionNo, int answerNo) {
+    public AnswerNotFoundException(int questionNo, int answerNo) {
         this.message = "질문 번호 " + questionNo + "번에 대한 답변 번호 " + answerNo + "을(를) 찾을 수 없습니다.";
     }
 
