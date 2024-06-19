bd3e2a977a312146198fdbeefc5a78ca435a3abf
SEO-YJ <tjdbwns19@naver.com>
2024-06-19 11:26:52 +0900

    refactor: 자체 서비스 포트폴리오 선택시 보유 펀드 조회를 위한 PortfolioController 수정
    FE로부터 portfolio_id를 전달 받아 해당 변수를 통해 private_portfolios_fund_products 테이블에서 데이터 조회하도록 개발

--- portfolio-service/src/main/java/com/pda/portfolio_service/controller/PortfolioController.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/controller/PortfolioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static com.pda.utils.api_utils.ApiUtils.error;
 import static com.pda.utils.api_utils.ApiUtils.success;
@@ -28,11 +29,17 @@ public class PortfolioController {
     }
 
     @GetMapping("/hitit/{portfolio_id}")
-    public ApiUtils.ApiResult getHititPortfoliosFund(@PathVariable("portfolio_id") String portfolio_id) {
-        if(portfolio_id.isBlank()){
+    public ApiUtils.ApiResult getHititPortfoliosFund(@PathVariable("portfolio_id") Integer portfolio_id) {
+        if(!isNumber(portfolio_id)){
             return error("포트폴리오 id가 전달되지 않음", HttpStatus.BAD_REQUEST);
         }
-        List<HititPortfoliosFundsResponseDto> hititPortfoliosFundsResponseDto = portfolioService.getHititPortfolios(portfolio_id);
+        List<HititPortfoliosFundsResponseDto> hititPortfoliosFundsResponseDto = portfolioService.getHititPortfoliosFunds(portfolio_id);
+
         return success(hititPortfoliosFundsResponseDto);
+    }
+
+    // 나중에 util - Validator로 이동
+    public static boolean isNumber(int num) {
+        return Pattern.matches("^[0-9]*$", Integer.toString(num));
     }
 }
