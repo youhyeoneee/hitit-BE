a880c7b04e934f6d237be4491c3690d0487c43ba
seohee99 <imshee99@gmail.com>
2024-06-20 16:05:38 +0900

    fix: 각 금융 상품 계좌 조회 기능 추가

--- asset-service/src/main/java/com/pda/asset_service/controller/AssetController.java
+++ asset-service/src/main/java/com/pda/asset_service/controller/AssetController.java
@@ -1,8 +1,8 @@
 package com.pda.asset_service.controller;
 
-import com.pda.asset_service.dto.MydataInfoDto;
-import com.pda.asset_service.dto.UserAccountInfoDto;
-import com.pda.asset_service.service.AssetServiceImpl;
+import com.pda.asset_service.dto.*;
+import com.pda.asset_service.jpa.SecurityAccount;
+import com.pda.asset_service.service.*;
 import com.pda.utils.api_utils.ApiUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,13 +17,48 @@ import java.util.List;
 public class AssetController {
 
     private final AssetServiceImpl assetService;
+    private final BankAccountServiceImpl bankAccountService;
+    private final SecurityAccountServiceImpl securityAccountService;
+    private final CardServiceImpl cardService;
+    private final PensionServiceImpl pensionService;
+    private final LoanServiceImpl loanService;
 
     @PostMapping("/mydata-link")
     public ApiUtils.ApiResult<List<MydataInfoDto>> linkMydata(@RequestBody UserAccountInfoDto userAccountInfoDto){
 
         List<MydataInfoDto> bankAccountsLinkInfo = assetService.linkMydata(userAccountInfoDto);
-
-
         return ApiUtils.success(bankAccountsLinkInfo);
+    }
+
+    @GetMapping("/bank-accounts")
+    public ApiUtils.ApiResult<List<BankAccountDto>> getBankAccounts(@RequestParam int userId){
+        List<BankAccountDto> bankAccounts = bankAccountService.getBankAccounts(userId);
+        return ApiUtils.success(bankAccounts);
+    }
+
+    @GetMapping("/security-accounts")
+    public ApiUtils.ApiResult<List<SecurityAccountDto>> getSecurityAccounts(@RequestParam int userId){
+        List<SecurityAccountDto> securityAccounts = securityAccountService.getSecurityAccounts(userId);
+        return ApiUtils.success(securityAccounts);
+    }
+
+    @GetMapping("/cards")
+    public ApiUtils.ApiResult<List<CardDto>> getCards(@RequestParam int userId){
+
+        List<CardDto> cards = cardService.getCards(userId);
+        return ApiUtils.success(cards);
+    }
+
+    @GetMapping("/pensions")
+    public ApiUtils.ApiResult<List<PensionDto>> getPensions(@RequestParam int userId){
+
+        List<PensionDto>  pensions = pensionService.getPensions(userId);
+        return ApiUtils.success(pensions);
+    }
+
+    @GetMapping("/loans")
+    public ApiUtils.ApiResult<List<LoanDto>> getLoans(@RequestParam int userId){
+        List<LoanDto> loans = loanService.getLoans(userId);
+        return ApiUtils.success(loans);
     }
 }
