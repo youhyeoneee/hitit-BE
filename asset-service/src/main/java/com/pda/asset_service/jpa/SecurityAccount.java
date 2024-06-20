06b99d12c2af554d7961800732450e437862c903
seohee99 <imshee99@gmail.com>
2024-06-20 12:30:57 +0900

    feat: 마이데이터 엔티티

---
+++ asset-service/src/main/java/com/pda/asset_service/jpa/SecurityAccount.java
@@ -0,0 +1,37 @@
+package com.pda.asset_service.jpa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "security_accounts")
+public class SecurityAccount {
+
+    @Id
+    @Column(name = "account_no")
+    private String accountNo;
+
+    @Column(name = "security_name")
+    private String securityName;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    private Integer balance;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private AssetUser assetUser;
+}
