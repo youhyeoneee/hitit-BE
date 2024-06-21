63e2a93ee73e6c7eef682ea08a1add3639efff47
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-21 15:47:02 +0900

    feat: (WIP) 노후 진단 테스트 결과 추가 API - 유형 타입 리스트로 수정
    #37

--- user-service/src/main/java/com/pda/retirements/dto/RetirementTestResponseDto.java
+++ user-service/src/main/java/com/pda/retirements/dto/RetirementTestResponseDto.java
@@ -4,21 +4,17 @@ import com.pda.retirements.jpa.RetirementType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 @Getter
 public class RetirementTestResponseDto {
-    Type type;
+    ArrayList<String> type;
 
     public RetirementTestResponseDto(RetirementType retirementType) {
-        this.type = new Type(retirementType.getName(), retirementType.getDescription());
+        this.type = new ArrayList<>(Arrays.asList(retirementType.getName(), retirementType.getDescription()));
     }
-
-    @Getter
-    @AllArgsConstructor
-    class Type {
-        private final String name;
-        private final String description;
-    }
-
 }
 
