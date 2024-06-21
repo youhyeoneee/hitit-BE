f2cfbd848444b1a0b5d7b9fa7e401e775c181fa7
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-21 14:01:19 +0900

    !HOTFIX: LoginDto에 기본 생성자 추가
    #52 #1

--- user-service/src/main/java/com/pda/user_service/dto/LoginDto.java
+++ user-service/src/main/java/com/pda/user_service/dto/LoginDto.java
@@ -5,9 +5,10 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class LoginDto {
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
