821927d339115a6e24d13c284be3fc9e904767db
seohee99 <imshee99@gmail.com>
2024-06-22 16:50:24 +0900

    feat: 컴포넌트 스캔 대상 변경

--- utils/src/main/java/com/pda/utils/UtilsApplication.java
+++ utils/src/main/java/com/pda/utils/UtilsApplication.java
@@ -3,7 +3,7 @@ package com.pda.utils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.pda")
 public class UtilsApplication {
 
 	public static void main(String[] args) {
