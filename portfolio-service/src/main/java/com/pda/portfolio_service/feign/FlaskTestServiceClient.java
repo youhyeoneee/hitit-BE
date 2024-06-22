34ef4aafad4c3b600a1cc0d665f10a0a271c4a44
SEO-YJ <tjdbwns19@naver.com>
2024-06-22 14:17:12 +0900

    feat: Flask와 통신을 테스트할 FlaskTestServiceClient 구현
    OpenFeign에 Flask test url 설정 후 테스트 진행

--- portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
@@ -1,14 +1,9 @@
 package com.pda.portfolio_service.feign;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @PropertySource(value = {"env.properties"})
 @FeignClient(name = "flaskClient", url = "${ml.client.url}")
-public interface PortfolioServiceClient {
+public interface FlaskTestServiceClient {
     @PostMapping("/sentiment")
     String getSentiment(@RequestHeader("Accept") String contentType, @RequestBody Map<String, Object> data);
 }
