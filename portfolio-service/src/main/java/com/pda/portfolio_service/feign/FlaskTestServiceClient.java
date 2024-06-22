cb33a1d02017b56547c110a84d2ce3ca84e895e1
SEO-YJ <tjdbwns19@naver.com>
2024-06-22 14:15:03 +0900

    docs: OpenFeign으로 통신할 url 설정
    env.properties에 저장 후, application.properties에 env.properties 설정

--- portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
@@ -1,6 +1,8 @@
 package com.pda.portfolio_service.feign;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,10 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-
-@FeignClient(name = "flaskClient", url = "http://127.0.0.1:5000/sentiment")
+@PropertySource(value = {"env.properties"})
+@FeignClient(name = "flaskClient", url = "${ml.client.url}")
 public interface PortfolioServiceClient {
-
-    @PostMapping
+    @PostMapping("/sentiment")
     String getSentiment(@RequestHeader("Accept") String contentType, @RequestBody Map<String, Object> data);
 }
