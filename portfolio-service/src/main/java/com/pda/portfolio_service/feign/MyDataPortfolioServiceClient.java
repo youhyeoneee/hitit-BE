6882eb110da1753af7e8866cd06bcb968c8faa5f
SEO-YJ <tjdbwns19@naver.com>
2024-06-24 15:52:25 +0900

    feat: ML 서버를 통해 마이데이터 포트폴리오를 제공 기능 구현
    OpenFeign을 통해 ML 서버에 마이데이터에 대한 포트폴리오 요청 개발

--- portfolio-service/src/main/java/com/pda/portfolio_service/feign/MyDataPortfolioServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/feign/MyDataPortfolioServiceClient.java
@@ -1,12 +1,18 @@
 package com.pda.portfolio_service.feign;
 
+import com.pda.portfolio_service.dto.MyDataFlaskLevelTest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
-@FeignClient(name = "mydataPortfolioClient", url = "${ml.server.url}")
+@FeignClient(name = "mydataPortfolioClient", url = "http://172.31.13.91:8080")
 public interface MyDataPortfolioServiceClient {
-    @GetMapping("/mydata/fundsrecom/{user_id}")
-    String getMyDataPortfolio(@RequestHeader("Accept") String contentType,
-                              @RequestParam Map<String, Integer> params);
+    @PostMapping("/mydata/funds")
+    MyDataFlaskResponseDto getMyDataPortfolio(@RequestHeader("Accept") String contentType,
+                                                    @RequestBody MyDataFundData myDataFundData);
+
+    @PostMapping("/mydata/fundss")
+    MyDataFlaskLevelTestResponseDto getMyDataLevelTestPortfolio(@RequestHeader("Accept") String contentType,
+                                                                @RequestBody MyDataFlaskLevelTest myDataFlaskLevelTest);
 }
