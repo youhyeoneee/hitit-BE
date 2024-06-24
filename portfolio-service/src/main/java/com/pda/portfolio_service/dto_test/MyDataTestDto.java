41841264a3c13bba78fbb0bdeabde13f0063d66f
SEO-YJ <tjdbwns19@naver.com>
2024-06-24 12:40:25 +0900

    test: OpenFeign 통신을 위한 FlaskTestServiceClient 구현
    local에서 Flask와 통신

---
+++ portfolio-service/src/main/java/com/pda/portfolio_service/dto_test/MyDataTestDto.java
@@ -0,0 +1,80 @@
+package com.pda.portfolio_service.dto_test;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.pda.portfolio_service.dto.HititPortfoliosFundsStocksAndBondsResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MyDataTestDto {
+
+    private String name;
+    private String investmentType;
+    private String summary;
+    private int minimumSubscriptionFee;
+    private int stockExposure;
+    private double return3m;
+    private List<FundDto> funds;
+
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+    public static class FundDto {
+
+        private String fundCode;
+        private String fundName;
+        private String fundTypeDetail;
+        private String companyName;
+        private double weight;
+        private double return3m;
+        // PortfolioFundAsset 관련 필드
+        private Float stock;
+        private Float stockForeign;
+        private Float bond;
+        private Float bondForeign;
+        private Float investment;
+        private Float etc;
+        private List<HititPortfoliosFundsStocksAndBondsResponseDto.FundStockDto> fundStocks;
+        private List<HititPortfoliosFundsStocksAndBondsResponseDto.FundBondDto> fundBonds;
+
+
+//        @Getter
+//        @Setter
+//        @AllArgsConstructor
+//        @NoArgsConstructor
+//        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+//        public static class FundStockDto {
+//
+//            private String stockName;
+//            private String size;
+//            private String style;
+//            private double weight;
+//        }
+//
+//        @Getter
+//        @Setter
+//        @AllArgsConstructor
+//        @NoArgsConstructor
+//        @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+//        public static class FundBondDto {
+//
+//            private String bondName;
+//            private String expiredDate;
+//            private double duration;
+//            private String credit;
+//            private double weight;
+//        }
+    }
+}
--- portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/feign/FlaskTestServiceClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @PropertySource(value = {"env.properties"})
-@FeignClient(name = "flaskClient", url = "${ml.client.url}")
+@FeignClient(name = "flaskClient", url = "http://172.31.16.127:8080")
 public interface FlaskTestServiceClient {
     @PostMapping("/sentiment")
     String getSentiment(@RequestHeader("Accept") String contentType, @RequestBody Map<String, Object> data);
