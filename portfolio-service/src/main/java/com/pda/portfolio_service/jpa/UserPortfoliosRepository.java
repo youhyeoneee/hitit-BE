6d4442f864f6475bba34ed29f5ada5e149811f65
SEO-YJ <tjdbwns19@naver.com>
2024-06-24 14:38:50 +0900

    feat: user_portfolios Repository 생성
    사용자가 보유한 포트폴리오를 조회하기 위한 Repository 생성

---
+++ portfolio-service/src/main/java/com/pda/portfolio_service/jpa/UserPortfoliosRepository.java
@@ -0,0 +1,9 @@
+package com.pda.portfolio_service.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserPortfoliosRepository extends JpaRepository<UserPortfolios, Integer> {
+    Optional<UserPortfolios> findByUserId(Integer userId);
+}
