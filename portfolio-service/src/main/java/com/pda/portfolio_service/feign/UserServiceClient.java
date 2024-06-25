497fd080ba009632f5bd90844d295eb90268930b
youhyeoneee <khukyh2018@khu.ac.kr>
2024-06-26 02:00:45 +0900

    fix: method 변경
    #78

--- portfolio-service/src/main/java/com/pda/portfolio_service/feign/UserServiceClient.java
+++ portfolio-service/src/main/java/com/pda/portfolio_service/feign/UserServiceClient.java
@@ -2,12 +2,12 @@ package com.pda.portfolio_service.feign;
 
 import com.pda.portfolio_service.dto.UserAgeTestScoreDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "user-service", url = "${service.url.user}")
 public interface UserServiceClient {
-    @PostMapping("/api/users/openfeign/{user_id}")
+    @GetMapping("/api/users/openfeign/{user_id}")
     UserAgeTestScoreDto getUserAgeTestScore(@PathVariable("user_id") int userId);
 
 }
